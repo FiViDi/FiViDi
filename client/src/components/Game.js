cc42f8e02f50f1e2ab88f06347e0d9b5dea9c14d
fionadrsnt <project.fionadarasanti@gmail.com>
2023-11-22 19:50:29 +0700

    game done chat on progress

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
+import Board from "./Board";
 
 function Game({ channel }) {
   const [playersJoined, setPlayersJoined] = useState(
     channel.state.watcher_count === 2
   );
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
   channel.on("user.watching.start", (event) => {
     setPlayersJoined(event.watcher_count === 2);
   });
   if (!playersJoined) {
     return <div> Waiting for other player to join...</div>;
   }
-  return <div>Game</div>;
+  return (
+    <div className="gameContainer">
+      <Board result={result} setResult={setResult} />
+      {/* chat */}
+      {/* leave game button */}
+    </div>
+  );
 }
 
 export default Game;
