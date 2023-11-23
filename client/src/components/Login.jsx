abd5264dcfdb1041bbc261f9ec7adac0de2db2a4
“RDHSAHS” <rdwikaharris@gmail.com>
2023-11-23 12:31:42 +0700

    Done, maybe

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import Axios from "axios";
 import Cookies from "universal-cookie";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAsync, updatePassword, updateUsername } from "../features/login/login-slice";
 
 function Login({ setIsAuth }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const { username, password, loading, error } = useSelector((state) => state.login);
+  const dispatch = useDispatch()
   const cookies = new Cookies();
+  useEffect(() => {
+    dispatch(fetchAsync())
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <section className="flex justify-center items-center" >
+          <h1>LOADING</h1>
+        </section>
+      </>
+    );
+  }
   const login = () => {
     Axios.post("http://localhost:3001/login", {
       username,
@@ -27,14 +45,14 @@ function Login({ setIsAuth }) {
       <input
         placeholder="Username"
         onChange={(event) => {
-          setUsername(event.target.value);
+          dispatch(updateUsername(event.target.value));
         }}
       />
       <input
         placeholder="Password"
         type="password"
         onChange={(event) => {
-          setPassword(event.target.value);
+          dispatch(updatePassword(event.target.value));
         }}
       />
       <button onClick={login}>Login</button>
